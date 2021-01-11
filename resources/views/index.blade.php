0ea96a1d41b97e003da40443685401b965c3b8f4
samidmedia <dimashalimgit@gmail.com>
2021-01-11 13:52:40 +0700

    memasukan codingan laravel keseluruhan

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout/main')
+@section('title', 'Web Programming UNPAS')
+    
+@section('container')
+      <div class="container">
+          <div class="row">
+              <div class="col-10">
+                  <h1 class="mt-3">Hello, World</h1>
+              </div>
+          </div>
+      </div>
+@endsection 
